bac12cc2d9df9323510c459ed60b12ad91e42095
Athul Jacob <athuljacobacaj@gmail.com>
2023-04-09 16:53:25 +0530

    added instal_prisma

---
+++ nodejs/install_prisma/hooks/post_gen.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:mason/mason.dart';
+
+Future<void> run(HookContext context) async {
+  final installProgress = context.logger.progress(
+      'running npm i --save-dev prisma typescript ts-node @types/node');
+  // Run `flutter packages get` after generation.
+  await Process.run('npm', [
+    'install',
+    '--save-dev',
+    'prisma',
+    'typescript',
+    'ts-node',
+    '@types/node',
+  ]);
+  installProgress.complete();
+  final generateProgress = context.logger
+      .progress('running npx prisma init --datasource-provider postgresql');
+  await Process.run(
+      'npx', ['prisma', 'generate', '--datasource-provider postgresql']);
+  generateProgress.complete();
+}
